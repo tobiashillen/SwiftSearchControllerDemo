9924631113a1781e32a527e3820e3d2bd58f6202
tobiashillen <tobias.hillen@gmail.com>
2017-02-21 09:54:19 +0100

    Initial Commit

---
+++ SearchControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchControllerDemo
+//
+//  Created by Tobias Hillén on 2017-02-21.
+//  Copyright © 2017 Tobias Hillén. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
